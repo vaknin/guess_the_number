ddac04b41052cb6a8270ab2ba715fed2376ebfda
Aviv <vaknins33@gmail.com>
2020-03-21 14:40:46 +0200

    final

---
+++ app.py
@@ -0,0 +1,76 @@
+# Imports
+import random, os
+
+# Globals
+global maxNum, guesses, guess
+clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+
+# Welcome message
+print("Welcome to 'Guess The Number'!")
+
+# Game Loop
+while True:
+
+    # Retrieve the maximum number
+    while True:
+        try:
+            maxNum = int(input("Please enter the maximum number possible: "))
+            break
+        except ValueError:
+            print("not a valid integer")
+
+    # Retrieve the number of guesses
+    while True:
+        try:
+            guesses = int(input("Please enter the number of guesses: "))
+            break
+        except ValueError:
+            print("not a valid integer")
+
+    # Generate the random number
+    randomNumber = random.randrange(1, maxNum + 1)
+    clear()
+
+    # Announce the game has started and explain rules
+    print(f"The number has been selected, please choose a number between 1 and {maxNum}.")
+
+    # Check for remaining guesses
+    while guesses > 0:
+
+        # Retrieve the user's guess
+        while True:
+            try:
+                guess = int(input("Make a guess: "))
+                break
+            except ValueError:
+                print("not a valid guess!")
+
+        # Clear screen
+        clear()
+
+        # Guess was correct
+        if guess == randomNumber:
+            print("Good Job! You've guessed the number!")
+            break
+
+        # Guess too high
+        elif guess > randomNumber:
+            print(f"{guess} is too high!")
+
+        # Guess too low
+        elif guess < randomNumber:
+            print(f"{guess} is too low!")
+
+        # Decrease number of guesses by 1
+        guesses = guesses - 1
+
+    # Game over
+    if guesses == 0:
+        clear()
+        print(f"Game over!\nThe number was {randomNumber}, good luck next time!")
+
+    # Play again?
+    choice = input("Would you like to play again? (Y/N)\n")
+    clear()
+    if choice != "Y" and choice != "y":
+        break
